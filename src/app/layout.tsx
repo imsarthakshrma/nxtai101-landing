533b308c04cbdbe9f9407db3049b04472f680ab3
imsarthakshrma <imsarthakshrma@gmail.com>
2025-10-23 14:02:54 +0530

    feat: add Meta Pixel tracking script and noscript fallback

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,6 +47,31 @@ export default function RootLayout({
             gtag('config', 'G-RQ5VML66YN');
           `}
         </Script>
+
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1330987888574044');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{display: 'none'}}
+            src="https://www.facebook.com/tr?id=1330987888574044&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </head>
       <body className={`font-sans ${inter.variable} ${instrumentSerif.variable}`}>
         <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
